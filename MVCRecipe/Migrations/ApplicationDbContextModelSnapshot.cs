483df914f520eb43295e4cdf27678232965f3d02
brendanlumpkin <blumpkin@crimson.ua.edu>
2019-12-01 21:14:45 -0600

    updated home search + categories page

--- MVCRecipe/Migrations/ApplicationDbContextModelSnapshot.cs
+++ MVCRecipe/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -4,20 +4,18 @@ using MVCRecipe.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace MVCRecipe.Data.Migrations
+namespace MVCRecipe.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20191029193230_InitialCreate")]
-    partial class InitialCreate
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.0.0")
+                .HasAnnotation("ProductVersion", "3.0.1")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
--- MVCRecipe/wwwroot/js/site.js
+++ MVCRecipe/wwwroot/js/site.js
@@ -8,7 +8,7 @@ function apiSearch() {
         "app_id": 'b9fca222',
         "app_key": '939b846b89040f57e5749e2bd374c8f1',
         "from": 0,
-        "to:": 15
+        "to:": 10
 
     };
     $.ajax({
@@ -17,19 +17,79 @@ function apiSearch() {
     })
         .done(function (data) {
             console.log(data);
-            var result;
-            for (var i = 1; i < 11; i++) {
-                result += "<p>" + data.hits[i].recipe.label + "</p><br>";
-                var count = 0;
-                while (data.hits[i].recipe.ingredientLines[count] != null) { 
-                    result += "<li>" + data.hits[i].recipe.ingredientsLines[count] + "</li><br>"
-                    count++;
-                }
-                results += "<p>" + data.hits[i].calories + "</p><br></br><br>"
-            }
-            $("#results").html(result);
-        })
-        .fail(function () {
-            alert("error");
+            for (var i = 0; i < 9; i++) {
+
+                var recipe = $("<div>");
+                var recipeTitle = $("<h5>");
+                var label = data.hits[i].recipe.label;
+                recipeTitle.text(label);
+                recipe.append(recipeTitle);
+
+                var recipeImg = $("<div>");
+                var img = $("<img>");
+                var imgAPI = data.hits[i].recipe.image;
+                img.attr("src", imgAPI);
+                recipeImg.append(img);
+                recipe.append(recipeImg);
+                var recipeContent = $("<div>");
+                var ingredients = $("<p>");
+                for (var j = 0; j < 20; j++) {
+                    var ingredient = data.hits[i].recipe.ingredientLines[j];
+                    var newIng = $("<p>");
+                    newIng.text(ingredient)
+                    ingredients.append(newIng);
+                };
+                recipeContent.append(ingredients);
+                recipe.append(recipeContent);
+                var link = $("<a>");
+                link.text("Complete Recipe");
+                var url = data.hits[i].recipe.url;
+                link.attr("href", url);
+                link.attr("target", "_blank");
+                recipe.append(link);
+                var save = $("<a>");
+                save.text("Save to Profile");
+                //save.attr("href", save function?);
+                //recipe.append(save);
+                recipe.append($("<h1>"));
+                $("#results").append(recipe);
+            };
         });
 }
+
+function categories(){
+    var categories = ["Breakfast", "Lunch", "Dinner"];
+    for (var x = 0; x < 3; x++) {
+        var params = {
+            "q": categories[x],
+            "app_id": 'b9fca222',
+            "app_key": '939b846b89040f57e5749e2bd374c8f1',
+            "from": 0,
+            "to:": 3
+        };
+        $.ajax({
+            url: 'https://api.edamam.com/search?' + $.param(params),
+            type: "GET",
+        })
+            .done(function (data) {
+                console.log(data);
+                var category = $("<div>");
+                var header = $("<h5>");
+                var name = data.q; //header not displaying
+                header.text(name); 
+                category.append(header);
+                for (var i = 0; i < 3; i++) {
+                    var title = data.hits[i].recipe.label;
+                    var url = data.hits[i].recipe.url;
+                    var link = $("<a>");
+                    link.text(title);
+                    link.attr("href", url);
+                    link.attr("target", "_blank");
+                    category.append(link);
+                    category.append($("<h1>"));
+                    $("#results").append(category);
+                }
+            });
+     }
+}
+    
